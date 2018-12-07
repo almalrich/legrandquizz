b9d75c4f91ede536e051a1f05e3f3cf80dff7e9c
almalrich <seb-bastos16@live.fr>
2018-12-07 13:48:59 +0100

    4eme commit

--- grandquizz.js
+++ grandquizz.js
@@ -1,59 +1,88 @@
-var arrObj = [{"question": "combien compte-t-on de 1ers Grands Cru Classés en France legis 1855?", "reponses": "3", "couleur": "vert"},
+var arrObj = [{
+    "question": "combien compte-t-on de 1ers Grands Cru Classés en France legis 1855?",
+    "reponses": "3",
+    "couleur": "vert"
+},/*tableau d'objets*/
     {"question": "Quel est la région du Fleurie?", "reponses": "4", "couleur": "rouge"},
     {"question": "Combien de fois est distillé le Cognac", "reponses": "2", "couleur": "bleu"},
     {"question": "qu'est ce que la philoxera", "reponses": "2", "couleur": "bleu"},
     {"question": "Quel est la couleur de Cheval Blanc St Emilion 1er cru classé?", "reponses": "4", "couleur": "rouge"},
-    {"question": "Quel est la quantité d'une bouteille standard de vin en France?", "reponses": "4", "couleur": "rouge"},
-    {"question": "De quelle couleur est la colerette d'une bouteille d'un A.O.C en France?", "reponses": "2","couleur": "bleu"},
+    {
+        "question": "Quel est la quantité d'une bouteille standard de vin en France?",
+        "reponses": "4",
+        "couleur": "rouge"
+    },
+    {
+        "question": "De quelle couleur est la colerette d'une bouteille d'un A.O.C en France?",
+        "reponses": "2",
+        "couleur": "bleu"
+    },
     {"question": "Quelles est la région d'un Riesling?", "reponses": "1", "couleur": "jaune"},
     {"question": "Quel est le vin primeur le plus connu?", "reponses": "3", "couleur": "vert"},
     {"question": "Quest-ce qu'un Charles de Gaulles?", "reponses": "4", "couleur": "rouge"}
-    ];
-let proposition = [{"repons1": "3", "repons2": "4", "repons3": "5", "repons4": "7","correct":"5"},
-    {"repons1": "bourgogne", "repons2": "bordeaux", "repons3": "côte du rhone", "repons4": "beaujolais", "correct": "beaujolais"},
+];
+let proposition = [{"repons1": "3", "repons2": "4", "repons3": "5", "repons4": "7", "correct": "5"},
+    {
+        "repons1": "bourgogne",
+        "repons2": "bordeaux",
+        "repons3": "côte du rhone",
+        "repons4": "beaujolais",
+        "correct": "beaujolais"
+    },
     {"repons1": "1", "repons2": "2", "repons3": "3", "repons4": "4", "correct": "2"},
-    {"repons1": "un champignon", "repons2": "un puceron", "repons3": "une araignée", "repons4": "une bactérie", "correct": "un puceron"},
+    {
+        "repons1": "un champignon",
+        "repons2": "un puceron",
+        "repons3": "une araignée",
+        "repons4": "une bactérie",
+        "correct": "un puceron"
+    },
     {"repons1": "blanc sec", "repons2": "blanc Moelleux", "repons3": "rosé", "repons4": "rouge", "correct": "rouge"},
     {"repons1": "35cl", "repons2": "50cl", "repons3": "70cl", "repons4": "75cl", "correct": "75cl"},
     {"repons1": "rouge", "repons2": "verte", "repons3": "jaune", "repons4": "noire", "correct": "verte"},
     {"repons1": "alsace", "repons2": "bourgogne", "repons3": "champagne", "repons4": "beaujolais", "correct": "Alsace"},
     {"repons1": "loire", "repons2": "bordeaux", "repons3": "beaujolais", "repons4": "alsace", "correct": "beaujolais"},
-    {"repons1": "une carafe de decantation", "repons2": "un couteau", "repons3": "une sorte de verre à boire", "repons4": "un tirebouchon", "correct": "un tirebouchon"}];
+    {
+        "repons1": "une carafe de decantation",
+        "repons2": "un couteau",
+        "repons3": "une sorte de verre à boire",
+        "repons4": "un tirebouchon",
+        "correct": "un tirebouchon"
+    }];
 
 
-let bonnereponse=0;
-let mauvaisereponse=0;
-let score=0;
-let clik=0;
-let i=0;
+let bonnereponse = 0;/*les variables*/
+let mauvaisereponse = 0;
+let score = 0;
+let clik = 0;
+let i = 0;
 let seconde = "00";
 let minute = "00";
 let temps1;
 
 
+/*le timer fonction temps*/
+function temps() {
+    $("#temps").html(minute + ":" + seconde);
+    seconde++;
+    temps1 = setTimeout(temps, 1000);
 
-function temps()
-    {
-        $("#temps").html(minute + ":" + seconde);
-        seconde++;
-        temps1=setTimeout(temps, 1000);
 
-
-        if (seconde == 60) {
-            minute++;
-            seconde = 0;
-        }
-
+    if (seconde == 60) {
+        minute++;
+        seconde = 0;
     }
 
-$("#time").click (function () {
+}
 
-temps();
+/*bouton start*/
+$("#time").click(function () {
 
+    temps();
 
 
 });
-
+/* on initialise le 1er objet pour initier*/
 $("#questions").html(arrObj[0].question);
 $("#a1").html(proposition[0].repons1);
 $("#a2").html(proposition[0].repons2);
@@ -61,42 +90,40 @@ $("#a3").html(proposition[0].repons3);
 $("#a4").html(proposition[0].repons4);
 
 
-$("#rep").hide();
+$("#rep").hide();/*on masque la div des resultat/reponse*/
 
-for (let l=1; l<=4; l++){
-    $("#a"+l).click(function () {
-
-
-        console.log("B :"+arrObj[i].reponses);
-console.log("U :"+ l);
-if (arrObj[i].reponses== l){
-    score++;
-    console.log("score : " +score);
-    bonnereponse++;
-
-}
-else{
-    mauvaisereponse++;
-    $("#bonnerep").append("pour la question"+" "+arrObj[i].question+" "+"la reponse était"+" "+proposition[i].correct+"<br>");
-    console.log("mauvaisereponse"+mauvaisereponse);
+for (let l = 1; l <= 4; l++) {/*boucle pour les autre questions/reponses*/
+    $("#a" + l).click(function () {
 
 
-}
+        console.log("B :" + arrObj[i].reponses);
+        console.log("U :" + l);
+        if (arrObj[i].reponses == l) {/*condition si gagne si perd*/
+            score++;
+            console.log("score : " + score);
+            bonnereponse++;
+
+        } else {
+            mauvaisereponse++;
+            $("#bonnerep").append("pour la question" + " " + arrObj[i].question + " " + "la reponse était" + " " + proposition[i].correct + "<br>");
+            console.log("mauvaisereponse" + mauvaisereponse);
+
+
+        }
 
         clik++;
         i++;
-        if(clik>9){
+        if (clik > 9) {/*condition de fin de partie*/
             $("#total").hide()
             $("#rep").show()
             resultat();
             clearTimeout(temps1);
-            $("#times").html(minute+":"+seconde);
+            $("#times").html(minute + ":" + seconde);
 
         }
 
 
-
-        $("#questions").fadeOut(1000, function(){
+        $("#questions").fadeOut(1000, function () {/*animation des question reponses jquery*/
 
             $("#questions").html(arrObj[i].question);
             $("#a1").html(proposition[i].repons1);
@@ -108,15 +135,14 @@ else{
         });
 
 
-
     });
 }
 
 
-function resultat () {
-    $("#bonnes").html("vous avez"+" "+ score+" "+"bonnes réponses");
-    $("#mauvaises").html("et vous avez"+" "+mauvaisereponse+" "+"mauvaises réponses");
+function resultat() {/*page des resultats*/
+    $("#bonnes").html("vous avez" + " " + score + " " + "bonnes réponses");
+    $("#mauvaises").html("et vous avez" + " " + mauvaisereponse + " " + "mauvaises réponses");
     $("#rep").show();
 }
 
-
+/*fin pour cette fois......*/
